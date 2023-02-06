217fa7a1a728b4e29468befd0c84b9cbdb2ea3bf
unknown <taemine.song@gmail.com>
2023-02-07 00:09:48 +1100

    updated home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,67 @@
+import { View, Text, SafeAreaView, Image, StatusBar, TextInput, ScrollView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from '../components/categories';
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2" style={{ marginTop:StatusBar.currentHeight }}>
+          <Image 
+            source={{
+              uri: 'https://links.papareact.com/wru'
+            }}
+            className="h-7 w-7 bg-gray-300"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+
+          <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput 
+            placeholder='Restaurants and cuisines'
+            keyboardType='default'  
+          />
+        </View>
+
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView>
+        {/* Categories */}
+            <Categories />
+        {/* Featured Rows */}
+      </ScrollView>
+            
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
