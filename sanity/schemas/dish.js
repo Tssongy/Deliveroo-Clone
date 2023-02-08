aba9cacff28a74b88ca6bf7c10fd9f427d9d01d1
unknown <taemine.song@gmail.com>
2023-02-09 00:03:08 +1100

    integrated Sanity for Content management

---
+++ sanity/schemas/dish.js
@@ -0,0 +1,15 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'dish',
+  title: 'Dish',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name of Dish",
+      validation: (Rule) => Rule.required()
+    }
+  ]
+})
---
+++ sanity/schemas/restaurant.js
@@ -0,0 +1,27 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Restaurant name",
+      validation: (Rule) => Rule.required(),
+    }
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return {...selection, subtitle: author && `by ${author}`}
+    },
+  },
+})
