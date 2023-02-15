468e7e5439ccbcb61cc0c17569d2a6ea5d7aea66
unknown <taemine.song@gmail.com>
2023-02-15 22:15:40 +1100

    updated restaurant screen and added a dishrow component

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,25 +1,105 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useRoute } from '@react-navigation/native';
+import { View, Text, ScrollView, Image, TouchableOpacity, StatusBar } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { urlFor } from '../sanity';
+import { ArrowLeftIcon, ChevronRightIcon, QuestionMarkCircleIcon, StarIcon } from 'react-native-heroicons/solid';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+  MapPinIcon
+} from "react-native-heroicons/outline";
+import DishRow from '../components/DishRow';
 
 const RestaurantScreen = () => {
-    const route = useRoute();
-    const {
-        id,
-        imgUrl,
-        title,
-        rating,
-        genre,
-        address,
-        short_description,
-        dishes,
-        long,
-        lat
-    } = route.params;
+  const navigation = useNavigation();
+  const route = useRoute();
+  const {
+      id,
+      imgUrl,
+      title,
+      rating,
+      genre,
+      address,
+      short_description,
+      dishes,
+      long,
+      lat
+  } = route.params;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <ScrollView style={{ marginTop:StatusBar.currentHeight }}>
+      <View className="relative">
+        <Image 
+          source={{
+            uri: urlFor(imgUrl).url()
+          }}
+          className="w-full h-56 bg-gray-300 p-4"
+
+        />
+        <TouchableOpacity 
+          onPress={navigation.goBack}
+          className="absolute top-2 left-5 p-2 bg-gray-100 rounded-full"
+        >
+          <ArrowLeftIcon size={20} color="#00CCBB" />
+        </TouchableOpacity>
+      </View>
+
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+          <Text className="text-3xl font-bold">{title}</Text>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> - {genre}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon color="gray" opacity={0.4} size={22} />
+              <Text className="text-xs text-gray-500">
+                Nearby - {address}
+              </Text>
+            </View>
+          </View>
+          <Text className="text-gray-500 mt-2 pb-4">
+            {short_description}
+          </Text>
+        </View>
+
+        <TouchableOpacity className="flex-row items-centre space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text>
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00CCBB" />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">
+          Menu
+        </Text>
+        {/* Dish rows */}
+        {dishes.map((dish) => (
+          <DishRow
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+          />
+        ))}
+      </View>
+    </ScrollView>
   )
 }
 
