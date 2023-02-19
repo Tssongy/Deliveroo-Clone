8f553f7c479e27416409cf43d2eecd4c25942a1e
Amy Song <amysong@192-168-1-116.tpgi.com.au>
2023-02-20 00:23:51 +1100

    added redux and basket state control

--- components/DishRow.js
+++ components/DishRow.js
@@ -10,10 +10,23 @@ import {
   MinusCircleIcon,
   PlusCircleIcon
 } from "react-native-heroicons/outline";
+import { useDispatch, useSelector } from 'react-redux';
+import { addToBasket, removeFromBasket, selectBasketItems, selectBasketItemsById } from '../slices/basketSlice';
 
 
 const DishRow = ({ id, name, description, price, image }) => {
   const [isPressed, setIsPressed] = useState(false);
+  const items = useSelector(state => selectBasketItemsById(state, id));
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }))
+  };
+
+  const removeItemFromBasket = () => {
+    if(items.length <= 0) return; 
+    dispatch(removeFromBasket({ id }));
+  }
 
   return (
     <>
@@ -45,14 +58,17 @@ const DishRow = ({ id, name, description, price, image }) => {
       {isPressed && (
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
-            <TouchableOpacity>
+            <TouchableOpacity 
+              onPress={removeItemFromBasket}
+              disabled={!items.length}
+            >
               <MinusCircleIcon 
-                color="#00CCBB"
+                color={items.length > 0 ? "#00CCBB": "gray"}
                 size={40}
               />
             </TouchableOpacity>
-            <Text>0</Text>
-            <TouchableOpacity>
+            <Text>{items.length}</Text>
+            <TouchableOpacity onPress={addItemToBasket}>
               <PlusCircleIcon 
                 color="#00CCBB"
                 size={40}
